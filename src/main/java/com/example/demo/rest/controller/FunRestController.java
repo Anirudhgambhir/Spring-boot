102049485c76ed924957d31c0f19f32d7f7df34c
Anirudh Gambhir <68347656+Anirudhgambhir@users.noreply.github.com>
2023-10-22 00:02:02 +0530

    Adding Log4j and lombok annotations

--- src/main/java/com/example/demo/rest/controller/FunRestController.java
+++ src/main/java/com/example/demo/rest/controller/FunRestController.java
@@ -1,20 +1,19 @@
 package com.example.demo.rest.controller;
 
 import com.example.demo.rest.Coach;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Log4j2
 public class FunRestController {
 
     private final Coach coach;
-
-    @Autowired
-    public FunRestController(@Qualifier("baseballCoachImpl") Coach coach) {
-        this.coach = coach;
-    }
 
     @GetMapping("/dailyWorkout")
     public String getDailyWorkout() {
