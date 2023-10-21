e8da894b3a25d5666a01776af1dd1541f7aed1da
Anirudh Gambhir <68347656+Anirudhgambhir@users.noreply.github.com>
2023-10-21 16:26:03 +0530

    Adding the following 1. Lazy Initialisation cricketCaoch 2. Adding Log4j2 Configuration, Testing with dummy logs

--- src/main/java/com/example/demo/rest/impl/BaseballCoachImpl.java
+++ src/main/java/com/example/demo/rest/impl/BaseballCoachImpl.java
@@ -1,11 +1,17 @@
 package com.example.demo.rest.impl;
 
 import com.example.demo.rest.Coach;
-import org.springframework.beans.factory.annotation.Qualifier;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 
 @Component
+@Log4j2
 public class BaseballCoachImpl implements Coach {
+
+    public BaseballCoachImpl() {
+        log.info("In Constructor {}", getClass().getSimpleName());
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Baseball practise in needed everyday";
--- src/main/java/com/example/demo/rest/impl/CricketCoachImpl.java
+++ src/main/java/com/example/demo/rest/impl/CricketCoachImpl.java
@@ -1,12 +1,21 @@
 package com.example.demo.rest.impl;
 
 import com.example.demo.rest.Coach;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
 @Primary
+@Lazy
+@Log4j2
 public class CricketCoachImpl implements Coach {
+
+    public CricketCoachImpl() {
+        log.info("In Constructor {}", getClass().getSimpleName());
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Practise Cricket Daily!!!!!!!!!!";
